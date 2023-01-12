a98cb990a9d9d2f03a654db603c0d3f86aa1b164
Orel-Viktor <OrelViktor93@gmail.com>
2023-01-12 16:26:21 +0200

    homework31

---
+++ counter/src/ui/components/App.jsx
@@ -0,0 +1,18 @@
+
+
+import '../../App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <p>
+        Clicked: <span id="value">0</span>
+        <button id="increment">+</button>
+        <button id="decrement">-</button>
+      </p>
+    </div>
+  )
+}
+
+export default App

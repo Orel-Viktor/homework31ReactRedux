cf06cc03ab5e7f8a5162a5aead2a4877802f59be
Orel-Viktor <OrelViktor93@gmail.com>
2023-01-15 21:56:39 +0300

    homework31-fix git commit -m  homework31-fix

--- todo/src/engine/todo-reducer/slice.js
+++ todo/src/engine/todo-reducer/slice.js
@@ -1,27 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const todos = createSlice({
-    name: "todos",
-    initialState: {
-        items: [],
-        loading: true,
-        todosValue: 0
+  name: "todos",
+  initialState: {
+    items: [],
+    loading: true,
+    todosValue: 0,
+  },
+  reducers: {
+    setItems: (state, action) => {
+      const { payload } = action;
+      state.items = payload;
+      state.todosValue = action.payload.length;
     },
-    reducers: {
-        setItems: (state, action) => {
-            const { payload } = action
-            state.items = payload
-            state.todosValue +=1
-        },
-        setLoading: (state, action) => {
-            const { payload } = action
-            state.loading = payload
-        },
+    setLoading: (state, action) => {
+      const { payload } = action;
+      state.loading = payload;
+    },
+  },
+  
+});
+// console.log (todos)
 
-    }
-})
-
-
-
-export const { setItems, setLoading } = todos.actions
-export const todoReducer = todos.reducer
+export const { setItems, setLoading } = todos.actions;
+export const todoReducer = todos.reducer;
